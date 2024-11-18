d927ee1c17984fc25b5ddc794a3b5311f3a6d7ff
emadmoenes <emadmoanes200@gmail.com>
2024-11-19 00:39:34 +0200

    handle side menu

--- lib/widgets/side_menu_widget.dart
+++ lib/widgets/side_menu_widget.dart
@@ -14,42 +14,50 @@ class _SideMenuWidgetState extends State<SideMenuWidget> {
   @override
   Widget build(BuildContext context) {
     final data = SideMenuData();
-    return Container(
-      child: ListView.builder(
-        itemCount: data.menu.length,
-        itemBuilder: (context, index) => buildMenuEntry(data, index),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 100, 10, 0),
+      child: Container(
+        child: ListView.builder(
+          itemCount: data.menu.length,
+          itemBuilder: (context, index) => buildMenuEntry(data, index),
+        ),
       ),
     );
   }
 
   Widget buildMenuEntry(SideMenuData data, int index) {
     final isSelected = selectedIndex == index;
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(6.0)),
-        color: isSelected ? selectionColor : Colors.transparent,
-      ),
-      child: InkWell(
-        onTap: () => setState(() {
-          selectedIndex = index;
-        }),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 7),
-              child: Icon(
-                data.menu[index].icon,
-                color: isSelected ? Colors.black : Colors.grey,
-              ),
-            ),
-            Text(
-              data.menu[index].title,
-              style: TextStyle(
-                  fontSize: 16,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(6.0)),
+          color: isSelected ? selectionColor : Colors.transparent,
+        ),
+        child: InkWell(
+          onTap: () => setState(() {
+            selectedIndex = index;
+          }),
+          child: Row(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 13, vertical: 7),
+                child: Icon(
+                  data.menu[index].icon,
                   color: isSelected ? Colors.black : Colors.grey,
-                  fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal),
-            )
-          ],
+                ),
+              ),
+              Text(
+                data.menu[index].title,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: isSelected ? Colors.black : Colors.grey,
+                    fontWeight:
+                        isSelected ? FontWeight.w600 : FontWeight.normal),
+              )
+            ],
+          ),
         ),
       ),
     );
