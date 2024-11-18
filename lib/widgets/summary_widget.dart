83a40ddca17e57ef859595654a3b282ccac98fe6
emadmoenes <emadmoanes200@gmail.com>
2024-11-19 00:34:41 +0200

    summary details

--- lib/widgets/summary_widget.dart
+++ lib/widgets/summary_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:responsive_project/widgets/pie_chart_widget.dart';
+import 'package:responsive_project/widgets/summary_details.dart';
 
 class SummaryWidget extends StatelessWidget {
   const SummaryWidget({super.key});
@@ -11,6 +12,15 @@ class SummaryWidget extends StatelessWidget {
       children: [
         SizedBox(height: 20),
         Chart(),
+        Text(
+          'Summary',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+        SizedBox(height: 16),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SummaryDetails(),
+        ),
       ],
     ));
   }
